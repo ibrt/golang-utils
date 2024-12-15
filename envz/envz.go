59b9081cd7e9cbd149bc7fc7d8e16b3def9b5b40
Ivan Bertona <ivan.bertona@gmail.com>
2024-12-15 16:49:03 +0100

    Add envz package.

---
+++ envz/envz.go
@@ -0,0 +1,73 @@
+package envz
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/ibrt/golang-utils/errorz"
+	"github.com/ibrt/golang-utils/memz"
+)
+
+// UnmarshalEnviron converts an OS-like environ to env map.
+func UnmarshalEnviron(environ []string, filterPrefix string) map[string]string {
+	env := make(map[string]string, len(environ))
+
+	for _, e := range environ {
+		p := strings.SplitN(e, "=", 2)
+
+		if len(p) == 2 && p[0] != "" && strings.HasPrefix(p[0], filterPrefix) {
+			env[p[0]] = p[1]
+		}
+	}
+
+	return env
+}
+
+// MarshalEnviron converts an env map to OS-like environ.
+func MarshalEnviron(env map[string]string) []string {
+	environ := make([]string, 0, len(env))
+
+	for k, v := range env {
+		environ = append(environ, fmt.Sprintf("%v=%v", k, v))
+	}
+
+	return environ
+}
+
+// MustSetenv is like [os.Setenv], but panics on error.
+func MustSetenv(key, value string) {
+	errorz.MaybeMustWrap(os.Setenv(key, value))
+}
+
+// MustUnsetenv is like [os.Unsetenv], but panics on error.
+func MustUnsetenv(key string) {
+	errorz.MaybeMustWrap(os.Unsetenv(key))
+}
+
+// WithEnv runs the closure after setting the given env, cleans up after.
+func WithEnv(env map[string]string, f func()) {
+	origEnv := make(map[string]*string)
+
+	defer func() {
+		for k, v := range origEnv {
+			if v != nil {
+				MustSetenv(k, *v)
+			} else {
+				MustUnsetenv(k)
+			}
+		}
+	}()
+
+	for k, v := range env {
+		if v, ok := os.LookupEnv(k); ok {
+			origEnv[k] = memz.Ptr(v)
+		} else {
+			origEnv[k] = nil
+		}
+
+		MustSetenv(k, v)
+	}
+
+	f()
+}
---
+++ envz/envz_test.go
@@ -0,0 +1,70 @@
+package envz_test
+
+import (
+	"os"
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	"github.com/ibrt/golang-utils/envz"
+	"github.com/ibrt/golang-utils/fixturez"
+)
+
+type Suite struct {
+	// intentionally empty
+}
+
+func TestSuite(t *testing.T) {
+	fixturez.RunSuite(t, &Suite{})
+}
+
+func (*Suite) TestEnviron(g *WithT) {
+	g.Expect(envz.UnmarshalEnviron(
+		[]string{
+			"PREFIX_K1=v1",
+			"PREFIX_K2=v2=x",
+			"K3=v3",
+		}, "PREFIX_")).
+		To(Equal(
+			map[string]string{
+				"PREFIX_K1": "v1",
+				"PREFIX_K2": "v2=x",
+			}))
+
+	g.Expect(envz.UnmarshalEnviron(
+		[]string{
+			"PREFIX_K1=v1",
+			"PREFIX_K2=v2=x",
+			"K3=v3",
+		}, "")).
+		To(Equal(
+			map[string]string{
+				"PREFIX_K1": "v1",
+				"PREFIX_K2": "v2=x",
+				"K3":        "v3",
+			}))
+
+	g.Expect(envz.MarshalEnviron(
+		map[string]string{
+			"PREFIX_K1": "v1",
+			"PREFIX_K2": "v2=x",
+			"K3":        "v3",
+		})).
+		To(ConsistOf(
+			"PREFIX_K1=v1",
+			"PREFIX_K2=v2=x",
+			"K3=v3"))
+}
+
+func (*Suite) TestWithEnv(g *WithT) {
+	g.Expect(os.Setenv("k1", "v11")).To(Succeed())
+
+	envz.WithEnv(map[string]string{"k1": "v12", "k2": "v2"}, func() {
+		g.Expect(os.Getenv("k1")).To(Equal("v12"))
+		g.Expect(os.Getenv("k2")).To(Equal("v2"))
+	})
+
+	g.Expect(os.Getenv("k1")).To(Equal("v11"))
+	_, ok := os.LookupEnv("k2")
+	g.Expect(ok).To(BeFalse())
+}
