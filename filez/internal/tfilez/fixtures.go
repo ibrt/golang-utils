0b62954007302937f0abbb33da99463acee309a2
Ivan Bertona <ivan.bertona@gmail.com>
2024-12-20 11:00:26 +0100

    More fixes.

--- filez/internal/tfilez/fixtures.go
+++ filez/internal/tfilez/fixtures.go
@@ -4,7 +4,9 @@ import (
 	"embed"
 )
 
-// FixturesEmbed provides an embedded FS for tests.
-//
-//go:embed fixtures
-var FixturesEmbed embed.FS
+var (
+	// FixturesEmbed provides an embedded FS for tests.
+	//
+	//go:embed fixtures
+	FixturesEmbed embed.FS
+)
